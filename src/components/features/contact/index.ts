d502bb72b9381c44fe04273644f98c00b1cf62f8
demaceo <hdemaceo@gmail.com>
2025-08-07 16:42:13 -0600

    gitignore tbc

--- src/components/features/contact/index.ts
+++ src/components/features/contact/index.ts
@@ -1,7 +1,5 @@
 // Contact feature components barrel exports
 export { default as ContactForm } from './ContactForm/ContactForm';
-export { default as FloatingContactButton } from './FloatingContactButton/FloatingContactButton';
 
 // Re-export types if needed
 export type * from './ContactForm/ContactForm';
-export type * from './FloatingContactButton/FloatingContactButton';
