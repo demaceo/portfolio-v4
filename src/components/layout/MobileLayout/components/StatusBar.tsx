b3ce5c4b9498922fe566d3ccc7fcb3917a048096
demaceo <hdemaceo@gmail.com>
2025-09-24 11:05:29 -0600

    enhane StatusBar stylings

--- src/components/layout/MobileLayout/components/StatusBar.tsx
+++ src/components/layout/MobileLayout/components/StatusBar.tsx
@@ -19,14 +19,36 @@ const StatusBar: React.FC<StatusBarProps> = ({ currentTime }) => {
 
   return (
     <div className="status-bar">
-      <Image
-        className="carrier"
-        alt="portfolio-logo"
-        src={`${ASSET_PATHS.LOGOS}/PORTFOLIO_LOGO.png`}
-        width={20}
-        height={20}
-      />
-      <span className="time">{formatTime(currentTime)}</span>
+      <div className="status-bar-left">
+        <Image
+          className="carrier-logo"
+          alt="portfolio-logo"
+          src={`${ASSET_PATHS.LOGOS}/PORTFOLIO_LOGO.png`}
+          width={24}
+          height={24}
+          priority
+        />
+        <span className="carrier-text">Demaceo</span>
+      </div>
+
+      <div className="status-bar-center">
+        <div className="notch-indicator"></div>
+      </div>
+
+      <div className="status-bar-right">
+        <div className="signal-bars">
+          <div className="bar bar-1"></div>
+          <div className="bar bar-2"></div>
+          <div className="bar bar-3"></div>
+          <div className="bar bar-4"></div>
+        </div>
+        <div className="wifi-icon">📶</div>
+        <div className="battery-container">
+          <div className="battery-level"></div>
+          <div className="battery-tip"></div>
+        </div>
+        <span className="time">{formatTime(currentTime)}</span>
+      </div>
     </div>
   );
 };
