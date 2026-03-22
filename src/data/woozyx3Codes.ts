26ea5d52dd611a1c52a72aa1509b5168c8885474
demaceo <hdemaceo@gmail.com>
2026-03-22 15:46:42 -0600

    Refactor styles in woozyx3 module and update friend codes for clarity and consistency

--- src/data/woozyx3Codes.ts
+++ src/data/woozyx3Codes.ts
@@ -6,20 +6,20 @@
 export const woozyx3Codes: Record<string, string> = {
     // Generated, human-readable codes for each friend. Replace the codes if
     // you'd prefer a different format or if you want them to be shorter.
-    "INTRO": "alright so i had intended on also sending out written messages on postcards to everyone who donated but after simply writing out the addresses on the letters, i realized that it would be much easier (and fun for me) to send a digital message instead. so here we go!",
+    "INTRO": "PSA: PLAN A: alright so i had initially intended on sending out these polaroids with hand-written postcards but 1) i worried that the polaroid wouldn't make the journey and 2) just writing out all of yalls addresses on each of the letters hurted my hand. Plan B: so we're doing this instead.",
     "WOOZYX3-RYAN": "Ryan — thanks for celebrating Woozy's third! Big woofs and thanks for the support.",
     "WOOZYX3-MATT": "Matt — happy Woozy day! Appreciate you donating and being awesome.",
-    "WOOZYX3-XOA": "Xoa — thanks for the love for Woozy's 3rd. You're the best.",
-    "WOOZYX3-NIKE": "Nike — thanks for celebrating Woozy! Sending a wet-nose thank you.",
-    "WOOZYX3-BRITTANY-SLATER": "Brittany S. — thanks so much for the birthday love. Woozy sends a wiggle.",
-    "WOOZYX3-ENRIQUE": "Enrique — thank you for the generous support and for celebrating Woozy's third!",
-    "WOOZYX3-KATY-K": "Katy K — big thanks for joining Woozy's party. Hope you like the polaroid!",
-    "WOOZYX3-ELS": "Els — thank you for donating and celebrating Woozy. Paw five!",
-    "WOOZYX3-LENNY": "Brittany — thanks a ton for the birthday gift to Woozy. You're lovely!",
-    "WOOZYX3-LYDIA": "Lydia — sending thanks from Woozy for your support and birthday wishes.",
-    "WOOZYX3-SARA": "Sara — thanks for helping make Woozy's day special. Big tail wags!",
-    "WOOZYX3-YOKA": "Yoka — appreciation and woofs for celebrating Woozy's 3rd with us.",
-    "WOOZYX3-JAKE-BUTTS": "Jake Butts — huge thanks for donating and celebrating Woozy. You're the best.",
+    "WOOZYX3XOA": "Xoa — thanks for the love for Woozy's 3rd. You're the best.",
+    "WOOZYX3NIKE": "Nike — thanks for celebrating Woozy! Sending a wet-nose thank you.",
+    "WOOZYX3SLATER": "Brittany S. — thanks so much for the birthday love. Woozy sends a wiggle.",
+    "WOOZYX3ENRIQUE": "Enrique — thank you for the generous support and for celebrating Woozy's third!",
+    "WOOZYX3KATY": "Katy K — big thanks for joining Woozy's party. Hope you like the polaroid!",
+    "WOOZYX3ELS": "Els — thank you for donating and celebrating Woozy. Paw five!",
+    "WOOZYX3LENNY": "Brittany — thanks a ton for the birthday gift to Woozy. You're lovely!",
+    "WOOZYX3LYDIA": "Lydia — sending thanks from Woozy for your support and birthday wishes.",
+    "WOOZYX3SARA": "Sara — thanks for helping make Woozy's day special. Big tail wags!",
+    "WOOZYX3YOKA": "Yoka — appreciation and woofs for celebrating Woozy's 3rd with us.",
+    "WOOZYX3FLO": "FLO — huge thanks for donating and celebrating Woozy. You're the best.",
 };
 
 // NOTE: This is an in-repo static mapping. For a production setup where
